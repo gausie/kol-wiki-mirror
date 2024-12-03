05965a60c22174566718068191a49fdc368eea74
Samuel Gaus <sam@gaus.co.uk>
2024-12-03 12:20:04 +0000

    Make pretty

--- importRecentChanges/sync.ts
+++ importRecentChanges/sync.ts
@@ -1,6 +1,6 @@
-import {execa} from "execa";
-import {format} from "date-fns";
-import {basename} from "node:path";
+import { execa } from "execa";
+import { format } from "date-fns";
+import { basename } from "node:path";
 
 type RecentChange = {
   type: "edit" | "new" | "log";
@@ -20,26 +20,30 @@ type RecentChange = {
 type RecentChanges = {
   query: {
     recentchanges: RecentChange[];
-  }
-}
+  };
+};
 
 type ExistingPage = {
-  pageid: number,
-  ns: number,
-  title: string,
+  pageid: number;
+  ns: number;
+  title: string;
   revisions: {
-    revid: number,
-    parentid: number,
-  }[]
+    revid: number;
+    parentid: number;
+  }[];
 };
 
 async function getChanges(): Promise<RecentChanges> {
-  const response = await fetch("https://kol.coldfront.net/thekolwiki/api.php?action=query&list=recentchanges&rcprop=title|ids|sizes|timestamp|user|userid|comment&rclimit=100&format=json");
+  const response = await fetch(
+    "https://kol.coldfront.net/thekolwiki/api.php?action=query&list=recentchanges&rcprop=title|ids|sizes|timestamp|user|userid|comment&rclimit=100&format=json",
+  );
   return await response.json();
 }
 
 async function getMirrorChanges(): Promise<RecentChanges> {
-  const response = await fetch("https://kol.ar.gy/api.php?action=query&list=recentchanges&rcprop=title|ids|sizes|timestamp|user|userid&rcnamespace=0|1&rclimit=100&format=json");
+  const response = await fetch(
+    "https://kol.ar.gy/api.php?action=query&list=recentchanges&rcprop=title|ids|sizes|timestamp|user|userid&rcnamespace=0|1&rclimit=100&format=json",
+  );
   return await response.json();
 }
 
@@ -53,30 +57,50 @@ function compareChanges(a: RecentChange, b: RecentChange) {
 }
 
 async function findPageInfo(c: RecentChange): Promise<ExistingPage> {
-  const response = await fetch(`https://kol.ar.gy/api.php?action=query&titles=${c.title}&prop=revisions&format=json`);
+  const response = await fetch(
+    `https://kol.ar.gy/api.php?action=query&titles=${c.title}&prop=revisions&format=json`,
+  );
   const result = await response.json();
-  return Object.values(result.query.pages as ExistingPage[]).filter((p) => p.ns === c.ns)[0]; 
+  return Object.values(result.query.pages as ExistingPage[]).filter(
+    (p) => p.ns === c.ns,
+  )[0];
 }
 
 async function exportRevision(c: RecentChange) {
-  const response = await fetch(`https://kol.coldfront.net/thekolwiki/api.php?action=query&format=json&export=1&revids=${c.revid}`);
-  const result = await response.json() as { query: { export: { "*": string } }};
+  const response = await fetch(
+    `https://kol.coldfront.net/thekolwiki/api.php?action=query&format=json&export=1&revids=${c.revid}`,
+  );
+  const result = (await response.json()) as {
+    query: { export: { "*": string } };
+  };
   let exported = result.query.export["*"];
 
   const existingPage = await findPageInfo(c);
 
   if (existingPage) {
-    exported = exported.replace(`<id>${c.pageid}</id>`, `<id>${existingPage.pageid}</id>`);
+    exported = exported.replace(
+      `<id>${c.pageid}</id>`,
+      `<id>${existingPage.pageid}</id>`,
+    );
   }
 
-  if (c.ns > 99) exported = exported.replace(`<ns>${c.ns}</ns>`,`<ns>${c.ns + 2900}</ns>`);
+  if (c.ns > 99)
+    exported = exported.replace(`<ns>${c.ns}</ns>`, `<ns>${c.ns + 2900}</ns>`);
 
   return exported;
 }
 
 async function getImageUrl(filePage: string) {
-  const response = await fetch(`https://kol.coldfront.net/thekolwiki/api.php?action=query&format=json&prop=imageinfo&iiprop=url&titles=${filePage}`);
-  const result = await response.json() as { query: { pages: { [pageid: number]: { pageid: number, imageinfo: { 0: { url: string }}}}} };
+  const response = await fetch(
+    `https://kol.coldfront.net/thekolwiki/api.php?action=query&format=json&prop=imageinfo&iiprop=url&titles=${filePage}`,
+  );
+  const result = (await response.json()) as {
+    query: {
+      pages: {
+        [pageid: number]: { pageid: number; imageinfo: { 0: { url: string } } };
+      };
+    };
+  };
 
   const pages = result.query.pages;
   if (!pages) throw new Error(`Could not discover url for file in ${filePage}`);
@@ -95,25 +119,36 @@ async function applyChanges(changes: RecentChange[]) {
       const url = (await getImageUrl(c.title)).replace(/^https/, "http");
       const filename = basename(url);
       await execa`docker exec -i kol-wiki-mediawiki-1 curl ${url} -o /var/dumps/imageImport/${filename}`;
-      const res = await execa`docker exec -i kol-wiki-mediawiki-1 /var/www/html/maintenance/run importImages ${[
-        "/var/dumps/imageImport",
-        "--comment",
-        c.comment.replace("\"", "\\\""),
-        "--timestamp",
-        c.timestamp,
-      ]}`;
+      const res =
+        await execa`docker exec -i kol-wiki-mediawiki-1 /var/www/html/maintenance/run importImages ${[
+          "/var/dumps/imageImport",
+          "--comment",
+          c.comment.replace('"', '\\"'),
+          "--timestamp",
+          c.timestamp,
+        ]}`;
       await execa`docker exec -i kol-wiki-mediawiki-1 rm /var/dumps/imageImport/${filename}`;
-      await execa({ input: c.title })`docker exec -i kol-wiki-mediawiki-1 /var/www/html/maintenance/run purgeList`
+      await execa({
+        input: c.title,
+      })`docker exec -i kol-wiki-mediawiki-1 /var/www/html/maintenance/run purgeList`;
       console.log(`${c.type} ${c.title}: ${res.stdout.split("\n")[2]}`);
     } else {
       const xml = await exportRevision(c);
-      const res = await execa({ input: xml })`docker exec -i kol-wiki-mediawiki-1 /var/www/html/maintenance/run importDump`;
+      const res = await execa({
+        input: xml,
+      })`docker exec -i kol-wiki-mediawiki-1 /var/www/html/maintenance/run importDump`;
       console.log(`${c.type} ${c.title}: ${res.stdout.split("\n")[0]}`);
     }
   }
 
-  const from = format(new Date(changes[changes.length - 1].timestamp).getTime() - 1000, "yyyyMMddHHmmss");
-  const to = format(new Date(changes[0].timestamp).getTime() + 1000, "yyyyMMddHHmmss");
+  const from = format(
+    new Date(changes[changes.length - 1].timestamp).getTime() - 1000,
+    "yyyyMMddHHmmss",
+  );
+  const to = format(
+    new Date(changes[0].timestamp).getTime() + 1000,
+    "yyyyMMddHHmmss",
+  );
 
   await execa`docker exec kol-wiki-mediawiki-1 /var/www/html/maintenance/run rebuildrecentchanges --from=${from} --to=${to}`;
   await execa`docker exec kol-wiki-mediawiki-1 /var/www/html/maintenance/run initSiteStats`;
@@ -125,9 +160,14 @@ async function main() {
 
   const changes = await getChanges();
 
-  const indexOfLatestMirrored = changes.query.recentchanges.findIndex((c) => compareChanges(c, latestMirrored));
+  const indexOfLatestMirrored = changes.query.recentchanges.findIndex((c) =>
+    compareChanges(c, latestMirrored),
+  );
 
-  const unmirrored = changes.query.recentchanges.slice(0, indexOfLatestMirrored);
+  const unmirrored = changes.query.recentchanges.slice(
+    0,
+    indexOfLatestMirrored,
+  );
 
   await applyChanges(unmirrored);
 }
